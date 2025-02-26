1445821ffa23de46c390f3197c504c752b32c6c8
Anderson Pereira <anderson.carro.95@gmail.com>
2025-02-26 00:27:29 -0300

    Finalizando a Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import Header from "./components/landing-page/header";
-import Hero from "./components/landing-page/hero";
-import Pricing from "./components/landing-page/pricing";
-import VideoExplanation from "./components/landing-page/video-explanation";
-
+import FAQ from './components/landing-page/faq';
+import Header from './components/landing-page/header';
+import Hero from './components/landing-page/hero';
+import Pricing from './components/landing-page/pricing';
+import VideoExplanation from './components/landing-page/video-explanation';
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className='max-w-7xl mx-auto'>
       <Header />
       <Hero />
       <VideoExplanation />
       <Pricing />
-      {/*
-      <FAQ /> */}
+      <FAQ />
     </div>
   );
 }
