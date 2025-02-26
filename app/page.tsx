790d5f9cc195c36a4b80538a085d6cd20467fa24
Anderson Pereira <anderson.carro.95@gmail.com>
2025-02-26 00:05:56 -0300

    Criando seções de Video e Pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
+import Pricing from "./components/landing-page/pricing";
+import VideoExplanation from "./components/landing-page/video-explanation";
 
 
 export default function Home() {
@@ -7,10 +9,9 @@ export default function Home() {
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
-      
-      {/*
       <VideoExplanation />
       <Pricing />
+      {/*
       <FAQ /> */}
     </div>
   );
